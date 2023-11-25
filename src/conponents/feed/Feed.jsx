24402b652eb5564cf33bcf98c7e2f7d45f4e1b6a
Central Scope <agent4scope@gmail.com>
2023-11-25 18:08:37 +0100

    dynamically adding post data to feed section

--- src/conponents/feed/Feed.jsx
+++ src/conponents/feed/Feed.jsx
@@ -2,20 +2,16 @@ import "./feed.css";
 // import conponents
 import Share from "../share/Share";
 import Post from "../post/Post";
+import { Posts } from "../../dummyData";
 
 const Feed = () => {
 	return (
 		<div className="feed">
 			<div className="feedWrapper">
 				<Share />
-				<Post />
-				<Post />
-				<Post />
-				<Post />
-				<Post />
-				<Post />
-				<Post />
-				<Post />
+				{Posts.map((p) => {
+					return <Post key={p.id} post={p} />;
+				})}
 			</div>
 		</div>
 	);
--- src/conponents/post/Post.jsx
+++ src/conponents/post/Post.jsx
@@ -1,7 +1,10 @@
 import { MoreVert } from "@mui/icons-material";
 import "./post.css";
+import { Users } from "../../dummyData";
 
-const Post = () => {
+const Post = ({ post }) => {
+	const user = Users.filter((u) => u.id === post.userId);
+	console.log(user[0].username);
 	return (
 		<div className="post">
 			<div className="postWrapper">
@@ -9,21 +12,19 @@ const Post = () => {
 					<div className="postTopLeft">
 						<img
 							className="postTopProfileImg"
-							src="/assets/profile-1.jpg"
+							src={user[0].profilePicture}
 							alt=""
 						/>
-						<span className="postUserName">
-							Favour Ikechukwu Uwadiegwu
-						</span>
-						<span className="postDate">5 mins ago</span>
+						<span className="postUserName">{user[0].username}</span>
+						<span className="postDate">{post.date}</span>
 					</div>
 					<div className="postTopRight">
 						<MoreVert />
 					</div>
 				</div>
 				<div className="postCenter">
-					<span className="postText">Hey it's my frist post :)</span>
-					<img className="postImg" src="/assets/b-1.jpg" alt="" />
+					<span className="postText">{post.desc}</span>
+					<img className="postImg" src={post.photo} alt="" />
 				</div>
 				<div className="postBottom">
 					<div className="postBottomLeft">
@@ -38,11 +39,13 @@ const Post = () => {
 							alt=""
 						/>
 						<span className="postLikeCounter">
-							32 people like it
+							{post.like} people like it
 						</span>
 					</div>
 					<div className="postBottomRight">
-						<div className="postCommentText">9 comments</div>
+						<div className="postCommentText">
+							{post.comment} comments
+						</div>
 					</div>
 				</div>
 			</div>
